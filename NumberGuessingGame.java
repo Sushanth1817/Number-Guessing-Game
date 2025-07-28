1f3aef02f9862f3a30917a9ffbae93994b4c2de6
Sushanth1817 <padamata.sresushanth3@gmail.com>
2025-07-28 10:20:42 +0530

    NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class NumberGuessingGame {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
@@ -13,12 +13,15 @@ public class Main {
         int count = 0;
         int guess;
 
-        System.out.printf("Guess a number from %d to %d:\n", min, max);
+        System.out.printf("Guess a number from %d to %d:", min, max);
+        System.out.println();
+        System.out.println("guess in 3 attempts for full score");
 
         do {
-            guess = sc.nextInt(); 
+            
             count++;
-
+            System.out.println("Attempt: "+count);
+            guess = sc.nextInt(); 
             if (guess > randomNumber) {
                 System.out.println("Guess too high, try a lower value.");
             } else if (guess < randomNumber) {
@@ -28,7 +31,23 @@ public class Main {
             }
 
         } while (guess != randomNumber);
-
-        System.out.printf("Attempts: %d\n", count);
+        System.out.println();
+        System.out.println();
+         if(count>0 && count<3){
+        System.out.printf("score 100 Congrats!!! on getting full score ;)  Attempts: %d\n", count);
+        System.out.println();
+         }
+         else if(count>3 && count<6){
+             System.out.printf("score 90 Attempts: %d\n", count);
+             System.out.println();
+         }
+         else if(count>6 && count <10){
+             System.out.printf("score 80 Attempts: %d\n", count);
+             System.out.println();
+         }
+         else{
+             System.out.printf("score 70 try again for a good score Attempts: %d\n", count);
+            System.out.println();
+                }
     }
 }
